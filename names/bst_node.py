08c86e5f47fe7fc71a83276c7aa3c0ce753692ba
Jensen Koch <jtkoch12@gmail.com>
2020-06-27 11:59:01 -0500

    added bst_node for contains function and finished names.py

---
+++ names/bst_node.py
@@ -0,0 +1,34 @@
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else: 
+                self.left = BSTNode(value)
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value) 
+
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if self.value > target:
+            if self.left is None:
+                return False
+            found = self.left.contains(target)
+        else:
+            if self.right is None:
+                return False
+            found = self.right.contains(target)
+        return found
